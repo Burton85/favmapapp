79c045238628fbdb6247d922e2f3ff87c6a4ab27
BurtonZH Hou <burtonzh.hou@ogilvy.com>
2025-07-20 19:14:24 +0800

    feat: Add HomePage and update App

--- App.tsx
+++ App.tsx
@@ -5,24 +5,11 @@
  * @format
  */
 
-import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
+import React from 'react';
+import HomePage from './src/components/HomePage';
 
 function App() {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  return (
-    <View style={styles.container}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <NewAppScreen templateFileName="App.tsx" />
-    </View>
-  );
+  return <HomePage />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-});
 
 export default App;
